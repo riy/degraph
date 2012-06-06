824134f8e744d43ccd5349bac6024865a4c8f8ba
Jens Schauder <jens.schauder@freenet.de>
2012-06-06 07:43:44 +0200

    first working executable jar

--- src/main/scala/de/schauderhaft/dependencies/Degraph.scala
+++ src/main/scala/de/schauderhaft/dependencies/Degraph.scala
@@ -1,18 +1,16 @@
 package de.schauderhaft.dependencies
-import de.schauderhaft.dependencies.analysis.Analyzer
-import de.schauderhaft.dependencies.writer.Writer
-import java.io.File
-import java.io.BufferedWriter
-import java.io.FileWriter
+
+import scala.util.matching.Regex
 import scala.xml.XML
+import de.schauderhaft.dependencies.analysis.Analyzer
+import de.schauderhaft.dependencies.categorizer.MultiCategorizer.combine
+import de.schauderhaft.dependencies.categorizer.PackageRegexpCategorizer.pattern
 import de.schauderhaft.dependencies.categorizer.InternalClassCategorizer
 import de.schauderhaft.dependencies.categorizer.PackageCategorizer
-import de.schauderhaft.dependencies.categorizer.MultiCategorizer.combine
-import de.schauderhaft.dependencies.filter.NoJdk
-import de.schauderhaft.dependencies.categorizer.PackageRegexpCategorizer.pattern
-import scala.util.matching.Regex
 import de.schauderhaft.dependencies.filter.IncludeExcludeFilter
 import de.schauderhaft.dependencies.filter.RegExpFilter
+import de.schauderhaft.dependencies.writer.Writer
+import filter.NoSelfReference
 
 /**
  * the main class of the DependencyManager, plugging everything together, starting the analysis process and writing the result to an XML file
@@ -21,8 +19,9 @@ object Degraph {
 
     def main(args : Array[String]) : Unit = {
         val config = CommandLineParser.parse(args)
+        val cat = buildCategorizer(config.groupings())
         val g = Analyzer.analyze(config.classpath(),
-            buildCategorizer(config.groupings()),
+            cat,
             buildFilter(config.includeFilter(), config.excludeFilter()))
 
         val xml = (new Writer()).toXml(g)
@@ -35,7 +34,8 @@ object Degraph {
         combine(categorizers : _*)
     }
 
-    private def buildFilter(includes : List[String], excludes : List[String]) = {
+    private def buildFilter(includes : List[String],
+                            excludes : List[String]) = {
         new IncludeExcludeFilter(
             includes.map((x : String) => RegExpFilter.filter(x.r)).toSet,
             excludes.map((x : String) => RegExpFilter.filter(x.r)).toSet)
