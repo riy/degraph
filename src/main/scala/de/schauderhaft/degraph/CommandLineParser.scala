c342608f6b75e1f39b15639b6712a48b73f3801f
Jens Schauder <jens.schauder@freenet.de>
2012-12-23 13:39:10 +0100

    made the command line parser slightly simpler
    ... by removing the inner class and replacing it with an annonymous subclass of LazyScallopConf

--- src/main/scala/de/schauderhaft/degraph/CommandLineParser.scala
+++ src/main/scala/de/schauderhaft/degraph/CommandLineParser.scala
@@ -7,30 +7,29 @@ import org.rogach.scallop.exceptions.Help
 
 object CommandLineParser {
 
-    class Conf(args: Seq[String]) extends LazyScallopConf(args) {
+    def parse(args: Array[String]) = {
+        new LazyScallopConf(args) {
 
-        version("Degraph prerelease. Use at own risk.")
-        banner("""Degraph analyses class and jar files and creates graphml documents out of it
+            version("Degraph prerelease. Use at own risk.")
+            banner("""Degraph analyses class and jar files and creates graphml documents out of it
 for visualizing dependencies.
 
 See https://github.com/schauder/Dependency-Manager for details""")
-        val output = opt[String]("output",
-            default = Some("output.graphml"),
-            descr = "the file name of the output file")
-        val classpath = opt[String]("classpath",
-            default = Some("."),
-            descr = "the classpath where degraph looks for classfiles to analyze")
-        val excludeFilter = opt[List[String]]("excludeFilter",
-            default = Some(List()),
-            descr = "if this argument is given those nodes that are matched by the given regular expression will get excluded from the graph.")
-        val includeFilter = opt[List[String]]("includeFilter",
-            default = Some(List()),
-            descr = "if this argument is given, only those nodes get included in the resulting graph, that match the given regular expression.")
-        val groupings = opt[List[String]]("groupings", default = Some(List()))
+            val output = opt[String]("output",
+                default = Some("output.graphml"),
+                descr = "the file name of the output file")
+            val classpath = opt[String]("classpath",
+                default = Some("."),
+                descr = "the classpath where degraph looks for classfiles to analyze")
+            val excludeFilter = opt[List[String]]("excludeFilter",
+                default = Some(List()),
+                descr = "if this argument is given those nodes that are matched by the given regular expression will get excluded from the graph.")
+            val includeFilter = opt[List[String]]("includeFilter",
+                default = Some(List()),
+                descr = "if this argument is given, only those nodes get included in the resulting graph, that match the given regular expression.")
+            val groupings = opt[List[String]]("groupings", default = Some(List()))
 
-    }
+        }
 
-    def parse(args: Array[String]) = {
-        new Conf(args)
     }
 }
