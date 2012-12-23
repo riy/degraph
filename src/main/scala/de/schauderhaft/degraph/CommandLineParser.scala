7c4089e53c05e33bd2bb3d140050b6ed286b582a
Jens Schauder <jens.schauder@freenet.de>
2012-12-23 09:51:52 +0100

    added descriptions to the command line arguments

--- src/main/scala/de/schauderhaft/degraph/CommandLineParser.scala
+++ src/main/scala/de/schauderhaft/degraph/CommandLineParser.scala
@@ -4,20 +4,31 @@ import org.rogach.scallop.exceptions.UnknownOption
 
 object CommandLineParser {
 
-    class Conf(args : Seq[String]) extends ScallopConf(args) {
+    class Conf(args: Seq[String]) extends ScallopConf(args) {
         version("Degraph prerelease. Use at own risk.")
         banner("""Degraph analyses class and jar files and creates graphml documents out of it
 for visualizing dependencies.
 
 See https://github.com/schauder/Dependency-Manager for details""")
-        val output = opt[String]("output", default = Some("output.graphml"))
-        val classpath = opt[String]("classpath", default = Some("."))
-        val excludeFilter = opt[List[String]]("excludeFilter", default = Some(List()))
-        val includeFilter = opt[List[String]]("includeFilter", default = Some(List()))
+        val output = opt[String]("output",
+            default = Some("output.graphml"),
+            descr = "the file name of the output file")
+        val classpath = opt[String]("classpath",
+            default = Some("."),
+            descr = "the classpath where degraph looks for classfiles to analyze")
+        val excludeFilter = opt[List[String]]("excludeFilter",
+            default = Some(List()),
+            descr = """if this argument is given those nodes that are matched by 
+                the given regular expression will get excluded from the graph.""")
+        val includeFilter = opt[List[String]]("includeFilter",
+            default = Some(List()),
+            descr = """if this argument is given, 
+                only those nodes get included in the resulting graph,
+                that match the given regular expression.""")
         val groupings = opt[List[String]]("groupings", default = Some(List()))
     }
 
-    def parse(args : Array[String]) = {
+    def parse(args: Array[String]) = {
         new Conf(args)
     }
 }
