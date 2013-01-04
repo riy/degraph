ee6293b22f1528b2fc2541f3accfbcb5d71d3f3b
Jens Schauder <jens.schauder@freenet.de>
2013-01-04 08:30:11 +0100

    implemented PackageCategorizer for degraph.analysis.Node

--- src/main/scala/de/schauderhaft/degraph/categorizer/PackageCategorizer.scala
+++ src/main/scala/de/schauderhaft/degraph/categorizer/PackageCategorizer.scala
@@ -1,11 +1,20 @@
 package de.schauderhaft.degraph.categorizer
 import com.jeantessier.dependency.ClassNode
+import de.schauderhaft.degraph.analysis.Node
 
 object PackageCategorizer extends Function1[AnyRef, AnyRef] {
-    def apply(value : AnyRef) = {
+    def apply(value: AnyRef) = {
         value match {
-            case cn : ClassNode => cn.getPackageNode()
-            case _              => value
+            case Node(t, n) if (t == "Class") => Node("Package", packagePart(n))
+            case _ => value
         }
+    }
+
+    def packagePart(name: String) = {
+        val lastDotIndex = name.lastIndexOf('.')
+        if (lastDotIndex >= 0)
+            name.substring(0, lastDotIndex)
+        else
+            name
     }
 }
