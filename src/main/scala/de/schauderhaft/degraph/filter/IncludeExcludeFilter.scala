f07f92a27689a4c35f516e550fdb19695248a921
Jens Schauder <jens.schauder@freenet.de>
2013-01-05 06:56:59 +0100

    Improved formatting

--- src/main/scala/de/schauderhaft/degraph/filter/IncludeExcludeFilter.scala
+++ src/main/scala/de/schauderhaft/degraph/filter/IncludeExcludeFilter.scala
@@ -1,14 +1,15 @@
 package de.schauderhaft.degraph.filter
 
 class IncludeExcludeFilter(
-    include : Set[AnyRef => Boolean],
-    exclude : Set[AnyRef => Boolean])
-        extends (AnyRef => Boolean) {
-    override def apply(x : AnyRef) = {
+    include: Set[AnyRef => Boolean],
+    exclude: Set[AnyRef => Boolean])
+    extends (AnyRef => Boolean) {
+
+    override def apply(x: AnyRef) = {
         include_?(x) && !exclude.exists(_(x))
     }
 
-    private def include_?(x : AnyRef) =
+    private def include_?(x: AnyRef) =
         if (include.isEmpty)
             true
         else include.exists(_(x))
