a46e80595c05b03d851cbf7837f095ef358bc2b1
Jens Schauder <jens.schauder@freenet.de>
2013-01-28 07:06:09 +0100

    renamed tests and prepared next test

--- src/test/scala/de/schauderhaft/degraph/categorizer/ParallelCategorizerTest.scala
+++ src/test/scala/de/schauderhaft/degraph/categorizer/ParallelCategorizerTest.scala
@@ -7,23 +7,19 @@ import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
 class ParallelCategorizerTest extends FunSuite with ShouldMatchers {
-    test("combine of zero functions returns the identity") {
+    test("parallel combination of zero functions returns the identity") {
         new ParallelCategorizer()("x") should be("x")
     }
-    test("combine of a single identity function returns the argument") {
+    test("parallel combination of a single identity function returns the argument") {
         new ParallelCategorizer(identity)("x") should be("x")
     }
 
-    test("combine of a single function returns function value of the argument") {
+    test("parallel combination of a single function returns function value of the argument") {
         new ParallelCategorizer(ListCategory("a", "b", "c"))("b") should be(ParentAwareNode("c"))
     }
 
-    test("combine of a single function returns cascading function value of the argument") {
+    test("parallel combination of two functions returns the results of the all functions (with identity") {
         pending
-        new ParallelCategorizer(ListCategory("a", "b", "c", "d"))("b") should be(ParentAwareNode("c", "d"))
+        new ParallelCategorizer(ListCategory("a", "b"), ListCategory("b", "c"))("a") should be(ParentAwareNode("b", "a"))
     }
-    //
-    //    test("combine of two functions returns the result of the first function if it is not equal to the argument") {
-    //        combine(ListCategory("a", "b"), ListCategory("b", "c"))("b") should be("c")
-    //    }
 }
