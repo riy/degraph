fe803bd9703c64d05ae71b41f09ccfc436046586
Jens Schauder <jens.schauder@freenet.de>
2013-03-15 05:48:50 +0100

    upgraded junit
    junit 4.11 deprecates the framework API so I switched that to org.junit. Should have been this way all the time

--- src/test/java/de/schauderhaft/degraph/java/JavaApiTest.java
+++ src/test/java/de/schauderhaft/degraph/java/JavaApiTest.java
@@ -1,8 +1,8 @@
 package de.schauderhaft.degraph.java;
 
 import static java.util.Arrays.asList;
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import java.util.HashSet;
 
