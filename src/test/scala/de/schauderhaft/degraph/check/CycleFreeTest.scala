ed9827664e50de86567279ec01ec39ad90a9b9b6
Jens Schauder <jens.schauder@freenet.de>
2013-03-29 11:14:37 +0100

    renamed test class to the apropriate name

--- src/test/scala/de/schauderhaft/degraph/check/CycleFreeTest.scala
+++ src/test/scala/de/schauderhaft/degraph/check/CycleFreeTest.scala
@@ -1,28 +1,33 @@
-package de.schauderhaft.degraph.graph
+package de.schauderhaft.degraph.check
 
 import org.junit.runner.RunWith
 import org.scalatest.matchers.ShouldMatchers
 import org.scalatest.FunSuite
 import org.scalatest.junit.JUnitRunner
-import NodeTestUtil.n
+import de.schauderhaft.degraph.graph.NodeTestUtil.n
+import de.schauderhaft.degraph.graph.Graph
 import de.schauderhaft.degraph.model.SimpleNode.classNode
 import de.schauderhaft.degraph.model.SimpleNode.packageNode
 import de.schauderhaft.degraph.slicer.PackageCategorizer
 import de.schauderhaft.degraph.slicer.ParallelCategorizer
 import de.schauderhaft.degraph.model.SimpleNode
+import de.schauderhaft.degraph.configuration.CycleFree
+import de.schauderhaft.degraph.graph.Graph
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class GraphCycleTest extends FunSuite with ShouldMatchers {
+class CycleFreeTest extends FunSuite with ShouldMatchers {
     test("an empty graph has no cycles") {
         val g = new Graph()
-        g.edgesInCycles should be(Set())
+        CycleFree.violations(g) should be(Set())
     }
 
     test("a graph with two cyclic dependent nodes without slices does not report those as cycles") {
         val g = new Graph()
         g.connect(n("a"), n("b"))
         g.connect(n("b"), n("a"))
-        g.edgesInCycles should be(Set())
+        CycleFree.violations(g) should be(Set())
     }
 
     test("a graph with a cyclic dependency between to packages returns dependencies between those packages as cyclic") {
@@ -31,7 +36,7 @@ class GraphCycleTest extends FunSuite with ShouldMatchers {
         g.connect(classNode("de.p2.B1"), classNode("de.p3.C2"))
         g.connect(classNode("de.p3.C1"), classNode("de.p1.A2"))
 
-        g.edgesInCycles should be(Set(
+        CycleFree.violations(g) should be(Set(
             (packageNode("de.p1"), packageNode("de.p2")),
             (packageNode("de.p2"), packageNode("de.p3")),
             (packageNode("de.p3"), packageNode("de.p1"))))
@@ -43,7 +48,7 @@ class GraphCycleTest extends FunSuite with ShouldMatchers {
         g.connect(classNode("de.p2.B1"), classNode("de.p3.C2"))
         g.connect(classNode("de.p3.C1"), classNode("de.p1.A2"))
 
-        g.edgesInCycles should be(Set(
+        CycleFree.violations(g) should be(Set(
             (packageNode("de.p1"), packageNode("de.p2")),
             (packageNode("de.p2"), packageNode("de.p3")),
             (packageNode("de.p3"), packageNode("de.p1"))))
