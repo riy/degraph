b6b599722690d580fde16a232938143bb20896c4
Jens Schauder <jens.schauder@freenet.de>
2013-04-10 06:57:19 +0200

    improved scaladoc comments

--- src/main/scala/de/schauderhaft/degraph/analysis/dependencyFinder/AnalyzerLike.scala
+++ src/main/scala/de/schauderhaft/degraph/analysis/dependencyFinder/AnalyzerLike.scala
@@ -4,6 +4,13 @@ import de.schauderhaft.degraph.graph.Graph
 import de.schauderhaft.degraph.model.Node
 import de.schauderhaft.degraph.model.Node
 
+/**
+ * trait abstracting over who or what provides dependencies.
+ *
+ *  Usage of this trait instead of it single current implementation
+ *  allows the implementation later to be replaced by something else,
+ *  like analyzers providing Spring or database dependencies.
+ */
 trait AnalyzerLike {
     def analyze(
         sourceFolder: String,
