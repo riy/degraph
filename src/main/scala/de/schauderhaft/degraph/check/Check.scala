11efa89a0d94b4dc7c782a34298dc4aecc36d1ce
Jens Schauder <jens.schauder@freenet.de>
2013-04-10 06:57:13 +0200

    improved scaladoc comments

--- src/main/scala/de/schauderhaft/degraph/check/Check.scala
+++ src/main/scala/de/schauderhaft/degraph/check/Check.scala
@@ -11,18 +11,39 @@ import de.schauderhaft.degraph.graph.Graph
 import org.scalatest.matchers.ShouldMatchers._
 import de.schauderhaft.degraph.configuration.LayeringConstraint
 
+/**
+ * provides access to configurations and scalatest matchers useful when testing for dependencies.
+ *
+ * With all members of Check imported a simple use case looks like this:
+ *
+ *     classpath.forType("module").allow("advertising", "billing", "customer") should be (violationFree)
+ *
+ * Such a test checks that
+ * * all slice types are cycle free.
+ * * for the slicetype *module* the slices *advertising* may depend on
+ * *billing* and *customer*, and *billing* may depend on *customer*.
+ * * Dependencies in other directions are not allowed.
+ *
+ * Other slices not mentioned in the constraint definition may depend on each other or on the slices in the constraint.
+ */
 object Check {
+
+    /**
+     * a Configuration object containing the current classpath.
+     *
+     * Intended as a starting point for analyzing the application that is currently running.
+     *
+     * Note in a typical maven like setup it will also include the test classes as well as all libraries, which might not be desirable.
+     * Manipulate the configuration to only contain the classpath elements required or use include and exclude filters for limiting the analyzed classes.
+     */
     val classpath = new Configuration(
         classpath = Option(System.getProperty("java.class.path")),
         analyzer = Analyzer)
 
-    private def sliceNode(edge: (Node, Node)) = {
-        val (n1, n2) = edge
-        n1.types == n2.types && n1.types.head != "Class"
-    }
-
-    private def lowerCaseFirstLetter(s: String) = s.head.toLower + s.tail
-
+    /**
+     * a matcher for Configurations testing if the classes specified in the configuration
+     * adhere to the dependency constraints configured in the configuration.
+     */
     val violationFree = new BeMatcher[Configuration] {
         def apply(conf: Configuration) = {
             val g = conf.createGraph()
@@ -45,5 +66,11 @@ object Check {
         }
     }
 
+    private def sliceNode(edge: (Node, Node)) = {
+        val (n1, n2) = edge
+        n1.types == n2.types && n1.types.head != "Class"
+    }
+
+    private def lowerCaseFirstLetter(s: String) = s.head.toLower + s.tail
 }
 
