d91cc4e106b1d6adf94f9f87de08095e746a477d
Thomas Michael <thomas.michael@anmi24.de>
2013-04-14 21:18:49 +0200

    added Java to build and create GuiStarter

---
+++ src/main/java/de/schauderhaft/degraph/java/gui/GuiStarter.java
@@ -0,0 +1,35 @@
+package de.schauderhaft.degraph.java.gui;
+
+import javafx.stage.Stage;
+
+/**
+ * open the degraph visualisation
+ * 
+ */
+public class GuiStarter extends javafx.application.Application {
+
+	public GuiStarter() {
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("degraph");
+		primaryStage.show();
+	}
+
+	public void show() {
+		launch(new String[0]);
+	}
+
+	/**
+	 * TODO: delete this entrypoint
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Geht");
+		GuiStarter s = new GuiStarter();
+		s.show();
+	}
+}
