3f4ea6dd7e7f01ded14bf680fd92cd5a25e3b80e
Thomas Michael <thomas.michael@anmi24.de>
2013-04-17 22:07:47 +0200

    convert method uses Labling-class

--- src/main/scala/de/schauderhaft/degraph/gui/NodeLabelConverter.java
+++ src/main/scala/de/schauderhaft/degraph/gui/NodeLabelConverter.java
@@ -5,21 +5,16 @@ import java.util.Set;
 
 import javafx.scene.control.Label;
 import de.schauderhaft.degraph.model.Node;
-import de.schauderhaft.degraph.model.SimpleNode;
+import de.schauderhaft.degraph.writer.Labeling;
 
 public class NodeLabelConverter {
 
 	public Set<Label> toLabel(Set<Node> nodes) {
 		Set<Label> result = new HashSet<>();
 		for (Node n : nodes) {
-			if (n instanceof SimpleNode) {
-				SimpleNode simpleNode = (SimpleNode) n;
-				result.add(new Label(simpleNode.name()));
-			} else {
-				result.add(new Label(n.toString()));
-			}
+			String nodeName = Labeling.apply(n, null);
+			result.add(new Label(nodeName));
 		}
 		return result;
 	}
-
 }
