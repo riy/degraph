fcb1097a8def09e70bebc87f4076a031292d4e47
KroneckerDelta <KroneckerDelta@anmi24.de>
2013-04-21 20:53:27 +0200

    changed the names of fx-components

--- src/main/scala/de/schauderhaft/degraph/gui/MainViewController.java
+++ src/main/scala/de/schauderhaft/degraph/gui/MainViewController.java
@@ -2,10 +2,13 @@ package de.schauderhaft.degraph.gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import de.schauderhaft.degraph.model.Node;
 
 public class MainViewController {
 	@FXML
@@ -15,23 +18,34 @@ public class MainViewController {
 	private URL location;
 
 	@FXML
-	private AnchorPane ContentView;
-
-	@FXML
-	private AnchorPane NodeView;
+	private AnchorPane contentView;
 
 	@FXML
 	private AnchorPane mainView;
 
 	@FXML
+	private Label nodeNameLabel;
+
+	@FXML
+	private AnchorPane nodeView;
+
+	@FXML
 	void onMouseClicked(MouseEvent event) {
+		System.out.println("KLickMainView");
 	}
 
 	@FXML
 	void initialize() {
-		assert ContentView != null : "fx:id=\"ContentView\" was not injected: check your FXML file 'MainView.fxml'.";
-		assert NodeView != null : "fx:id=\"NodeView\" was not injected: check your FXML file 'MainView.fxml'.";
+		assert contentView != null : "fx:id=\"contentView\" was not injected: check your FXML file 'MainView.fxml'.";
 		assert mainView != null : "fx:id=\"mainView\" was not injected: check your FXML file 'MainView.fxml'.";
+		assert nodeNameLabel != null : "fx:id=\"nodeNameLabel\" was not injected: check your FXML file 'MainView.fxml'.";
+		assert nodeView != null : "fx:id=\"nodeView\" was not injected: check your FXML file 'MainView.fxml'.";
+
+		Set<Node> topNodes = DataProvider.getTopNodes();
+		assert topNodes != null : "no data";
+
+		// Label nodeName = DataProvider.getNodeName(topNodes);
+		// nodeNameLabel = nodeName;
 
 	}
 }
