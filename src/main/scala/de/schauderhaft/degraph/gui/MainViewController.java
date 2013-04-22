9b23fed7bd19d9bbdab357a876b18523c7bd6015
KroneckerDelta <KroneckerDelta@anmi24.de>
2013-04-22 18:42:34 +0200

    View contains one controller for MainView and one controller for a node

--- src/main/scala/de/schauderhaft/degraph/gui/MainViewController.java
+++ src/main/scala/de/schauderhaft/degraph/gui/MainViewController.java
@@ -1,12 +1,18 @@
 package de.schauderhaft.degraph.gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import java.util.Set;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+
+import org.apache.log4j.Logger;
+
 import de.schauderhaft.degraph.model.Node;
 
 /**
@@ -25,6 +31,8 @@ public class MainViewController {
 	@FXML
 	private AnchorPane mainView;
 
+	Logger LOG = Logger.getLogger(MainViewController.class);
+
 	@FXML
 	void onMouseClicked(MouseEvent event) {
 		System.out.println("KLickMainView");
@@ -36,6 +44,20 @@ public class MainViewController {
 
 		Set<Node> topNodes = DataProvider.getTopNodes();
 		assert topNodes != null : "no data";
+
+		FXMLLoader loader = new FXMLLoader();
+		try {
+			loader.setLocation(this.location);
+			Parent node = (Parent) loader.load(getClass().getResource(
+					"NodeViewTemplate.fxml").openStream());
+
+			Object controller = loader.getController();
+			assert controller != null;
+
+			mainView.getChildren().add(node);
+		} catch (IOException e) {
+			LOG.error(e);
+		}
 
 	}
 }
