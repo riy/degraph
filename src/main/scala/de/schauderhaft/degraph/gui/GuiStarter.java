3fa56367c966f1daed692206c6b26162ae51eaf6
KroneckerDelta <KroneckerDelta@anmi24.de>
2013-04-22 22:46:39 +0200

    added Chess Testdata

--- src/main/scala/de/schauderhaft/degraph/gui/GuiStarter.java
+++ src/main/scala/de/schauderhaft/degraph/gui/GuiStarter.java
@@ -4,8 +4,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import de.schauderhaft.degraph.java.ChessCategory;
 import de.schauderhaft.degraph.java.JavaGraph;
 import de.schauderhaft.degraph.java.JavaHierarchicGraph;
+import de.schauderhaft.degraph.model.SimpleNode;
 
 /**
  * open the degraph visualisation
@@ -43,9 +45,19 @@ public class GuiStarter extends javafx.application.Application {
 	public static void main(String[] args) {
 		GuiStarter s = new GuiStarter();
 
-		JavaGraph simpleGraph = new JavaGraph();
+		JavaGraph graph = new JavaGraph(new ChessCategory());
+		graph.connect(node("King"), node("Queen"));
+		graph.connect(node("Queen"), node("Rook"));
+		graph.connect(node("Rook"), node("Bishop"));
+		graph.connect(node("Rook"), node("Knight"));
+		graph.connect(node("Knight"), node("Pawn"));
+		graph.connect(node("Bishop"), node("Pawn"));
 
-		s.show(simpleGraph);
+		graph.save("chess.graphml");
+		s.show(graph);
 	}
 
+	private static SimpleNode node(String s) {
+		return new SimpleNode(s, s);
+	}
 }
