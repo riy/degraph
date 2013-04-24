98f6c26423777055fa9837211e927f08a4e61073
KroneckerDelta <KroneckerDelta@anmi24.de>
2013-04-24 20:28:31 +0200

    moved main to testclass

--- src/main/scala/de/schauderhaft/degraph/gui/GuiStarter.java
+++ src/main/scala/de/schauderhaft/degraph/gui/GuiStarter.java
@@ -37,27 +37,4 @@ public class GuiStarter extends javafx.application.Application {
 		launch(new String[0]);
 	}
 
-	/**
-	 * TODO: delete this entrypoint
-	 * 
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		GuiStarter s = new GuiStarter();
-
-		JavaGraph graph = new JavaGraph(new ChessCategory());
-		graph.connect(node("King"), node("Queen"));
-		graph.connect(node("Queen"), node("Rook"));
-		graph.connect(node("Rook"), node("Bishop"));
-		graph.connect(node("Rook"), node("Knight"));
-		graph.connect(node("Knight"), node("Pawn"));
-		graph.connect(node("Bishop"), node("Pawn"));
-
-		graph.save("chess.graphml");
-		s.show(graph);
-	}
-
-	private static SimpleNode node(String s) {
-		return new SimpleNode(s, s);
-	}
 }
