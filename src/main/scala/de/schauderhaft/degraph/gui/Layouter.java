e2ade4cc53f1594c2b8bc35a9fbe8cd8c2248ebb
KroneckerDelta <KroneckerDelta@anmi24.de>
2013-05-13 06:47:46 +0200

    move layout to layoutclass

--- src/main/scala/de/schauderhaft/degraph/gui/Layouter.java
+++ src/main/scala/de/schauderhaft/degraph/gui/Layouter.java
@@ -1,5 +1,9 @@
 package de.schauderhaft.degraph.gui;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javafx.scene.layout.AnchorPane;
 import de.schauderhaft.degraph.java.JavaHierarchicGraph;
 import de.schauderhaft.degraph.model.Node;
 
@@ -28,6 +32,43 @@ public class Layouter {
 				notYetImplementedY);
 		notYetImplementedX += 160;
 
+		return result;
+	}
+
+	/**
+	 * Layout a set of nodes and returns a set of their controller.
+	 */
+	public Set<NodeController> layoutedChildren(Set<Node> nodes) {
+		Set<NodeController> result = new HashSet<>();
+		for (Node node : nodes) {
+			// Set<Node> childrenOfParent = graph.topNodes(); // testcode TODO:
+			// delete thie
+			Set<Node> childrenOfParent = graph.contentsOf(node);
+
+			NodeController parentController = new NodeController(node);
+
+			AnchorPane parentContentPane = (AnchorPane) parentController
+					.lookup(NodeController.PANE_NAME);
+			for (Node childrenNode : childrenOfParent) {
+
+				NodeController childController = new NodeController(
+						childrenNode);
+				parentContentPane.getChildren().add(childController);
+
+				childController.setLayout(nextPosition(childrenNode));
+			}
+
+			NodePosition positionForParent = nextPosition(node);
+			System.out.println("Position: " + positionForParent);
+			parentController.setLayout(positionForParent);
+			parentController.fitToSize();
+			System.out.println("position for Parent: " + node.name() + " :"
+					+ positionForParent + " Size: "
+					+ parentController.getPrefHeight() + ","
+					+ parentController.getPrefWidth());
+
+			result.add(parentController);
+		}
 		return result;
 	}
 }
--- src/main/scala/de/schauderhaft/degraph/gui/MainViewController.java
+++ src/main/scala/de/schauderhaft/degraph/gui/MainViewController.java
@@ -1,20 +1,15 @@
 package de.schauderhaft.degraph.gui;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.ResourceBundle;
-import java.util.Set;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import de.schauderhaft.degraph.gui.util.FXMLUtil;
 import de.schauderhaft.degraph.java.JavaHierarchicGraph;
-import de.schauderhaft.degraph.model.Node;
 
 /**
  * Controller for the main window which includes 0-n nodeViews
@@ -43,44 +38,11 @@ public class MainViewController extends ScrollPane {
 	@FXML
 	void initialize() {
 
+		// pane as content for this scrollPane
 		Pane pane = new StackPane();
 
-		Set<Node> topNodes = new HashSet<>();
-		ArrayList<Node> arrayList = new ArrayList<>(graph.topNodes());
-		topNodes.add(arrayList.get(1));
-		// topNodes.add(arrayList.get(2));
-
 		Layouter layout = new Layouter(graph);
-		for (Node parent : topNodes) {
-			// Set<Node> childrenOfParent = graph.contentsOf(parent);
-			Set<Node> childrenOfParent = graph.topNodes();
-			// Set<Node> childrenOfParent = new HashSet<>();
-			// ArrayList<Node> arrayList = new ArrayList<>(graph.topNodes());
-			// childrenOfParent.add(arrayList.get(1));
-			// childrenOfParent.add(arrayList.get(2));
-			NodeController parentController = new NodeController(parent);
-
-			AnchorPane parentContentPane = (AnchorPane) parentController
-					.lookup(NodeController.PANE_NAME);
-			for (Node childrenNode : childrenOfParent) {
-
-				NodeController childController = new NodeController(
-						childrenNode);
-				parentContentPane.getChildren().add(childController);
-
-				childController.setLayout(layout.nextPosition(childrenNode));
-			}
-
-			NodePosition positionForParent = layout.nextPosition(parent);
-			System.out.println("Position: " + positionForParent);
-			parentController.setLayout(positionForParent);
-			pane.getChildren().add(parentController);
-			parentController.fitToSize();
-			System.out.println("position for Parent: " + parent.name() + " :"
-					+ positionForParent + " Size: "
-					+ parentController.getPrefHeight() + ","
-					+ parentController.getPrefWidth());
-		}
+		pane.getChildren().addAll(layout.layoutedChildren(graph.topNodes()));
 
 		this.setContent(pane);
 	}
