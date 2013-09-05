391b97a6c9f7869dc69f729da370f16eff365f90
Jens Schauder <jens.schauder@freenet.de>
2013-09-05 21:01:43 +0200

    fixed the version mentioned in the command line parser. This should have happened before the release.

--- src/main/scala/de/schauderhaft/degraph/configuration/CommandLineParser.scala
+++ src/main/scala/de/schauderhaft/degraph/configuration/CommandLineParser.scala
@@ -9,7 +9,7 @@ object CommandLineParser {
     def parse(args: Seq[String]) = {
         new LazyScallopConf(args) {
 
-            version("Degraph Version 0.1.0.")
+            version("Degraph Version 0.0.5")
             banner("""Degraph analyses class and jar files and creates graphml documents out of it
 for visualizing dependencies.""")
             footer("""See https://github.com/schauder/degraph for more information""")
