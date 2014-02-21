bce07ddb386352ad33495f20ee5820fc6656c737
Jens Schauder <jens.schauder@freenet.de>
2014-02-21 06:57:06 +0100

    moved files for test in subdir
    this way the test does not find all the classes of degraph itself

--- src/test/scala/de/schauderhaft/degraph/analysis/asm/FileFinderTest.scala
+++ src/test/scala/de/schauderhaft/degraph/analysis/asm/FileFinderTest.scala
@@ -5,23 +5,23 @@ import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.Matchers._
 import java.io.File
-import org.junit.BeforeClass
 import org.scalatest.BeforeAndAfterAll
-import org.scalatest.prop.PropertyChecks
 
 @RunWith(classOf[JUnitRunner])
 class FileFinderTest extends FunSuite
   with BeforeAndAfterAll {
 
-  val userDir = new File(System.getProperty("user.dir")).getCanonicalPath()
-  val subDir = new File(userDir, "subdir")
+  val userDir = new File(System.getProperty("user.dir"))
+  val tempDir = new File(userDir, "fileFinderTestDir")
+  val subDir = new File(tempDir, "subdir")
 
   val filesForTest = Set("A.class", "J.jar", "T.txt")
 
   override def beforeAll() = {
+    tempDir.mkdir()
     // create some Files to be used in the test
     for (f <- filesForTest) {
-        new File(userDir, f).createNewFile()
+        new File(tempDir, f).createNewFile()
     }
 
     subDir.mkdir()
@@ -32,37 +32,36 @@ class FileFinderTest extends FunSuite
 
   override def afterAll() = {
     for (f <- filesForTest) {
-      new File(userDir, f).delete()
+      new File(tempDir, f).delete()
       new File(subDir, f).delete()
     }
     subDir.delete()
   }
 
   test("FileFinder returns an empty Set if it finds nothing") {
-    new FileFinder("/thisPathShouldNotExist/").find() should be(Set[File]())
+    new FileFinder(new File(tempDir,"/thisPathShouldNotExist/").getAbsolutePath).find() should be(Set[File]())
   }
 
   test("FileFinder should find File in user dir when searching for file in current directory") {
-    new FileFinder(".").find() should contain(new File(userDir, "A.class"))
+    new FileFinder(tempDir.getAbsolutePath).find() should contain(new File(tempDir, "A.class"))
   }
 
   test("FileFinder should only find class or jar files") {
-    new FileFinder(".").find().foreach(
+    new FileFinder(tempDir.getAbsolutePath).find().foreach(
       f => f.getName() should (endWith(".class") or endWith(".jar")))
   }
 
   test("FileFinder should find the class file") {
-    new FileFinder(".").find()
+    new FileFinder(tempDir.getAbsolutePath).find()
       .filter(_.getName().endsWith(".class")) should not be ('empty)
   }
 
   test("FileFinder should find the jar file") {
-    new FileFinder(".").find()
+    new FileFinder(tempDir.getAbsolutePath).find()
       .filter(_.getName().endsWith(".jar")) should not be ('empty)
   }
 
   test("FileFinder should find File in subdir dir when searching for file in current directory") {
-    new FileFinder(".").find() should contain(new File(new File(userDir, "subdir"), "A.class"))
+    new FileFinder(tempDir.getAbsolutePath).find() should contain(new File(new File(tempDir, "subdir"), "A.class"))
   }
-
 }
