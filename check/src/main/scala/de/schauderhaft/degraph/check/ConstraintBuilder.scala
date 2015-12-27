de82680ca944b9504999b5bb2cfa42926f92277a
Jens Schauder <jens.schauder@freenet.de>
2015-12-27 07:57:40 +0100

    Constraint Builder now accepts a function for filtering the classpath

--- check/src/main/scala/de/schauderhaft/degraph/check/ConstraintBuilder.scala
+++ check/src/main/scala/de/schauderhaft/degraph/check/ConstraintBuilder.scala
@@ -1,16 +1,12 @@
 package de.schauderhaft.degraph.check
 
-import de.schauderhaft.degraph.configuration.Configuration
-import de.schauderhaft.degraph.configuration.Constraint
-import de.schauderhaft.degraph.configuration.Pattern
-import de.schauderhaft.degraph.configuration.UnnamedPattern
-import de.schauderhaft.degraph.configuration.NamedPattern
+import de.schauderhaft.degraph.configuration.{Configuration, Constraint, NamedPattern, Pattern, UnnamedPattern}
+
 import scala.annotation.varargs
-import scala.util.matching.Regex
 
 /**
- * the basis for the DSL do define constraints on your dependencies in tests.
- */
+  * the basis for the DSL do define constraints on your dependencies in tests.
+  */
 case class ConstraintBuilder(
   private val config: Configuration = new Configuration(),
   sliceType: String = "",
@@ -18,7 +14,8 @@ case class ConstraintBuilder(
   excludes: Seq[String] = Seq(),
   slicings: Map[String, Seq[Pattern]] = Map(),
   constraints: Set[Constraint] = Set(),
-  output: Option[String] = None) {
+  output: Option[String] = None
+) {
 
   def printTo(path: String) = copy(output = Some(path))
 
@@ -30,66 +27,74 @@ case class ConstraintBuilder(
   }
 
   private def modifyConfig(slices: IndexedSeq[AnyRef], toConstraint: (String, IndexedSeq[Layer]) => Constraint): ConstraintBuilder =
-    copy(constraints =
-      constraints +
-        toConstraint(sliceType, slices.map((x: AnyRef) => any2Layer(x))))
+    copy(
+      constraints =
+        constraints +
+          toConstraint(sliceType, slices.map((x: AnyRef) => any2Layer(x)))
+    )
 
   /**
-   * allows a chain of dependencies. Any slice specified as an argument may depend on any slice mentioned later in the list of arguments.
-   * Dependencies in the opposite direction are forbidden and lead to a test failure
-   *
-   * Arguments are either Strings, denoting a slice or a {Layer}
-   *
-   * see also #allowDirect
-   */
+    * allows a chain of dependencies. Any slice specified as an argument may depend on any slice mentioned later in the list of arguments.
+    * Dependencies in the opposite direction are forbidden and lead to a test failure
+    *
+    * Arguments are either Strings, denoting a slice or a {Layer}
+    *
+    * see also #allowDirect
+    */
   @varargs def allow(slices: AnyRef*): ConstraintBuilder =
     modifyConfig(slices.toIndexedSeq, LayeringConstraint)
 
   /**
-   *  allows a slice mentioned in the argument list to depend on the directly next element in the argument list.
-   *
-   *
-   *
-   *  see also #allow
-   */
+    * allows a slice mentioned in the argument list to depend on the directly next element in the argument list.
+    *
+    *
+    *
+    * see also #allow
+    */
   @varargs def allowDirect(slices: AnyRef*): ConstraintBuilder =
     modifyConfig(slices.toIndexedSeq, DirectLayeringConstraint)
 
   @varargs def withSlicing(sliceType: String, sls: AnyRef*) = {
-    copy(sliceType = sliceType, slicings = slicings + (sliceType ->
-      sls.map {
-        case s: String => UnnamedPattern(s)
-        case (n: String, p: String) => NamedPattern(n, p)
-        case p: Pattern => p
-      }))
+    copy(
+      sliceType = sliceType, slicings = slicings + (sliceType ->
+        sls.map {
+          case s: String => UnnamedPattern(s)
+          case (n: String, p: String) => NamedPattern(n, p)
+          case p: Pattern => p
+        })
+    )
   }
 
   def including(s: String): ConstraintBuilder = copy(includes = includes :+ s)
+
   def excluding(s: String): ConstraintBuilder = copy(excludes = excludes :+ s)
 
   /**
-   * removes all jar files from the classpath to be analyzed.
-   *
-   * Everything ending in .jar is considered a jar file.
-   */
+    * removes all jar files from the classpath to be analyzed.
+    *
+    * Everything ending in .jar is considered a jar file.
+    */
   def noJars: ConstraintBuilder =
-    copy(config = config.copy(classpath = config.classpath.map(noJars)))
+    filterClasspath(!_.endsWith(".jar"))
 
   /**
-   * reduces the classpath to be analyzed to include only those elements matching the pattern.
-   *
-   * The pattern is can contain * as a wildcard.
-   */
+    * reduces the classpath to be analyzed to include only those elements matching the pattern.
+    *
+    * The pattern is can contain * as a wildcard.
+    */
   def filterClasspath(pattern: String): ConstraintBuilder = {
     val regexp = pattern.replace("*", ".*").r
-    copy(config = config.copy(classpath = config.classpath.map(filterClasspath(regexp, _))))
+    val filter: (String) => Boolean = regexp.pattern.matcher(_).matches()
+    filterClasspath(filter)
   }
 
-  private def filterClasspath(pattern: Regex, s: String) =
-    filterClasspathString(s, pattern.pattern.matcher(_).matches())
-
-  private def noJars(s: String) =
-    filterClasspathString(s, ! _.endsWith(".jar"))
+  /**
+    * reduces the classpath to be analyzed to include only those elements matching the filter.
+    *
+    */
+  def filterClasspath(filter: (String) => Boolean): ConstraintBuilder = {
+    copy(config = config.copy(classpath = config.classpath.map(filterClasspathString(_, filter))))
+  }
 
   private def filterClasspathString(s: String, filter: String => Boolean) = {
     val sep = System.getProperty("path.separator")
